e63cc551b3f5697ded7aea64616e767838ee32b7
ShaileshKumar007 <shailesh1429k@gmail.com>
2022-07-27 10:02:21 +0530

    Adding the taking command functionality

--- virtual assistant.py
+++ virtual assistant.py
@@ -28,3 +28,22 @@ def wishMe():
         speak("Good Evening!")  
 
     speak("I am Jarvis Sir. Please tell me how may I help you")       
+
+def takeCommand():
+    #Takes microphone input from the user and returns string output
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")    
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        print("Say that again please...")  
+        return "None"
+    return query
